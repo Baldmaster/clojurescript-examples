3fbbcc004d8a39aefd32e004c2be4fd3f3bee8df
Baldmaster <sergey.archlinux@gmail.com>
2016-01-27 08:54:16 +0200

    jvm options

--- physics/project.clj
+++ physics/project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
+  :jvm-opts ["-Xmx64m"]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [org.clojure/core.async "0.2.374"]]
